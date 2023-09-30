7813011ac618366c3e03a92a87552d383a527f3b
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-09-30 20:22:06 +0500

    home page updates

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { ScrollingTextComponent } from './scrolling-text/scrolling-text.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeroSectionComponent,
+    FooterComponent,
+    ScrollingTextComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeroSectionComponent,
+    FooterComponent,
+    ScrollingTextComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.page';
 import { HomeRoutingModule } from './home-routing.module';
+import { ComponentsModule } from "../../components/components.module";
 
 
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+    declarations: [
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        ComponentsModule
+    ]
 })
 export class HomeModule { }
