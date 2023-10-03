344788f058e8aa64a5d891acd447873cbd1811d6
umerinsaan <umerinsaan@gmail.com>
2023-10-03 19:12:43 +0500

    do some documentation for the code.

--- src/app/pages/aboutus/aboutus.page.ts
+++ src/app/pages/aboutus/aboutus.page.ts
@@ -8,11 +8,20 @@ import { AboutUsPerson } from './models/aboutus-person.model';
 })
 export class AboutusComponent {
 
+  // An array to hold information about people involved in the organization.
   people: AboutUsPerson[] = [];
+
+  // An array defining the possible roles within the organization.
   roles: string[] = ['Faculty', 'Excom', 'Team Leads', 'Team XYZ'];
 
+  /**
+   * Constructor - Initializes the component.
+   *
+   * In the constructor, dummy data is created to populate the 'people' array.
+   * This data is used to render the information on the 'About Us' page.
+   */
   constructor() {
-    //Creating dummy data for now, just to render it.
+    // Creating a dummy data entry for a Faculty member.
     this.people.push({
       name: 'Name',
       role: 'Faculty',
@@ -20,6 +29,7 @@ export class AboutusComponent {
       imagePath: 'icons/user.svg'
     });
 
+    // Creating dummy data for Excom, Team Leads, and Team XYZ roles.
     for (let i = 0; i < 9; i++) {
       this.people.push({
         name: 'Name',
@@ -42,6 +52,15 @@ export class AboutusComponent {
     }
   }
 
+  /**
+   * getFilteredByRole - Filter people based on their roles.
+   *
+   * This method filters the 'people' array to return only those individuals
+   * who have the specified 'role'.
+   *
+   * @param {string} role - The role by which to filter the people.
+   * @returns {AboutUsPerson[]} - An array of people matching the specified role.
+   */
   getFilteredByRole(role: string): AboutUsPerson[] {
     return this.people.filter((person) => person.role === role);
   }
